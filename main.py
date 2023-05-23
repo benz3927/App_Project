b0df515ff105abb83ce241892ad2ad881a1446fe
benz3927 <benz3927@gmail.com>
2023-05-23 11:43:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn import svm
 from sklearn import metrics
+from io import StringIO
+
+from sklearn.feature_selection import chi2
+import numpy as np
 
 # Load the dataset
 df = pd.read_csv("complaints.csv")
 
+col = ['Product', 'Consumer complaint narrative']
+df = df[col]
+df = df[pd.notnull(df['Consumer complaint narrative'])]
+df.columns = ['Product', 'Consumer_complaint_narrative']
+df['category_id'] = df['Product'].factorize()[0]
+category_id_df = df[['Product', 'category_id']].drop_duplicates().sort_values('category_id')
+category_to_id = dict(category_id_df.values)
+id_to_category = dict(category_id_df[['category_id', 'Product']].values)
+df.head()
 
-# Preprocessing the data
-df = df[['text', 'airline_sentiment']]
-df = df[df.airline_sentiment != 'neutral']
+fig = plt.figure(figsize=(8,6))
+df.groupby('Product').Consumer_complaint_narrative.count().plot.bar(ylim=0)
+plt.show()
 
-# Split the dataset into training and testing sets
-X = df.text
-y = df.airline_sentiment
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+features = tfidf.fit_transform(df.Consumer_complaint_narrative).toarray()
+labels = df.category_id
+features.shape
 
-# Feature extraction using TF-IDF
-vectorizer = TfidfVectorizer()
-X_train = vectorizer.fit_transform(X_train)
-X_test = vectorizer.transform(X_test)
+N = 2
+for Product, category_id in sorted(category_to_id.items()):
+  features_chi2 = chi2(features, labels == category_id)
+  indices = np.argsort(features_chi2[0])
+  feature_names = np.array(tfidf.get_feature_names())[indices]
+  unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
+  bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
+  print("# '{}':".format(Product))
+  print("  . Most correlated unigrams:\n. {}".format('\n. '.join(unigrams[-N:])))
+  print("  . Most correlated bigrams:\n. {}".format('\n. '.join(bigrams[-N:])))
 
-# Train the Support Vector Machine (SVM) model
-classifier = svm.SVC(kernel='linear')
-classifier.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(df['Consumer_complaint_narrative'], df['Product'], random_state = 0)
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(X_train)
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+clf = MultinomialNB().fit(X_train_tfidf, y_train)
 
-# Make predictions on the test set
-y_pred = classifier.predict(X_test)
 
-# Evaluate the model
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+
+
